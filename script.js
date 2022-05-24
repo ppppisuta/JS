4c11e9ea167e59ff26ce8857089ede3e157a876e
ppppisuta <100999242+ppppisuta@users.noreply.github.com>
2022-05-24 23:00:26 +0900

    インラインスタイル編

--- script.js
+++ script.js
@@ -1,15 +1,18 @@
 window.addEventListener('load' , function(){
     const pullDownButton = document.getElementById("lists")
-})
 
-lists.addEventListener('mouseover', function(){
+pullDownButton.addEventListener('mouseover', function(){
     console.log("カーソルが乗りました")
+    pullDownButton.setAttribute("style", "background-color:blue;")
 })
 
-lists.addEventListener('mouseout', function(){
+pullDownButton.addEventListener('mouseout', function(){
     console.log("カーソルが離れました")
+    pullDownButton.setAttribute("style", "background-color:red;")
 })
 
-lists.addEventListener('click', function(){
+pullDownButton.addEventListener('click', function(){
     console.log("クリックしました")
+    pullDownButton.setAttribute("style", "background-color:green;")
+})
 })
